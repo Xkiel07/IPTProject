742610dc68cff8e62e243287bc926e4a08c2d44e
Xkiel07 <101669970+Xkiel07@users.noreply.github.com>
2025-03-23 14:51:07 +0800

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install PHP and required extensions
+sudo apt-get update
+sudo apt-get install -y php8.2 php8.2-cli php8.2-mysql php8.2-zip php8.2-mbstring php8.2-xml php8.2-gd
+
+# Install Composer
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+
+# Install Laravel dependencies
+composer install --optimize-autoloader --no-dev
+
+# Start the Laravel development server
+php artisan serve --host=0.0.0.0 --port=10000
