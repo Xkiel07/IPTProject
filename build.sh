16c84036784a38f1343857464bb36196e4713609
Xkiel07 <101669970+Xkiel07@users.noreply.github.com>
2025-03-23 14:35:28 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 # Install PHP and required extensions
-sudo apt-get update
-sudo apt-get install -y php8.2 php8.2-cli php8.2-mysql php8.2-zip php8.2-mbstring php8.2-xml php8.2-gd
+apt-get update
+apt-get install -y php8.2 php8.2-cli php8.2-mysql php8.2-zip php8.2-mbstring php8.2-xml php8.2-gd
 
 # Install Composer
-curl -sS https://getcomposer.org/installer | php
-sudo mv composer.phar /usr/local/bin/composer
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 
 # Install Laravel dependencies
 composer install --optimize-autoloader --no-dev
 
-# Install npm dependencies (optional, for front-end assets)
+# Install npm dependencies
 npm install
 
-# Build assets (optional, for front-end assets)
+# Build assets
 npm run build
 
 # Start the Laravel development server
