73be89b62d41e0eaa918513075294682dc4e1f2d
Xkiel07 <101669970+Xkiel07@users.noreply.github.com>
2025-04-17 05:06:10 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,19 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';    
+import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-            ],
+            input: ['resources/css/app.css'],
             refresh: true,
         }),
-        tailwindcss(),
     ],
-    resolve:{
-        alias:{
-            '@': '/resources',
-            '$': 'jquery'
-        }
-    }
+    css: {
+        postcss: {
+            plugins: [
+                tailwindcss(),
+            ],
+        },
+    },
 });
