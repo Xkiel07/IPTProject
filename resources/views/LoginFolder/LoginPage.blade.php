000b2ca62174786c1b341155162d2faa831fbbad
Xkiel07 <101669970+Xkiel07@users.noreply.github.com>
2025-04-18 13:29:09 +0800

    Update LoginPage.blade.php

--- resources/views/LoginFolder/LoginPage.blade.php
+++ resources/views/LoginFolder/LoginPage.blade.php
@@ -145,6 +145,6 @@
 
 </body>
     {{-- Javascript --}}
-    <script src="{{asset('/javascript/Adminpopup.js')}}" ></script>
-    <script src="{{asset('/javascript/Staffpopup.js')}}" ></script>
+    <script src="{{asset('javascript/Adminpopup.js')}}" ></script>
+    <script src="{{ asset('javascript/Staffpopup.js') }}"></script>
 </html>
