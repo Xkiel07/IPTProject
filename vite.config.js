67200e4754884e91993239ff70462bffb6f21f82
Xkiel07 <101669970+Xkiel07@users.noreply.github.com>
2025-04-18 16:03:56 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,33 +2,26 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    // Base path for serving assets, adjust if necessary
-
-
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',  // Make sure this path matches the actual file location
-                'resources/js/app.js'     // Same here
+                'resources/css/app.css',
+                'resources/js/app.js',
             ],
             refresh: true,
         }),
     ],
-
     optimizeDeps: {
         include: [
             '@fortawesome/fontawesome-free',
             'bootstrap'
         ]
     },
-
     build: {
-        manifest: true,  // Generates the manifest file for asset versioning
-
-        // Rollup options to control output file names
+        manifest: true,
+        outDir: 'public/build', // <== IMPORTANT
         rollupOptions: {
             output: {
-                // Define asset file names to be hashed for cache-busting
                 assetFileNames: 'assets/[name]-[hash][extname]',
                 entryFileNames: 'assets/[name]-[hash].js',
             }
