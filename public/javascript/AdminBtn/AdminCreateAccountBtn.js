b41b84c7c1647602d61e09fe1ec7356b8c3b31ba
Xkiel07 <101669970+Xkiel07@users.noreply.github.com>
2025-04-20 20:46:23 +0800

    Update AdminCreateAccountBtn.js

--- public/javascript/AdminBtn/AdminCreateAccountBtn.js
+++ public/javascript/AdminBtn/AdminCreateAccountBtn.js
@@ -1,3 +1,3 @@
 function redirectToAdminCreateAccount() {
-    window.location.href = "http://localhost:8000/Create-Account";
+    window.location.href = "https://iptproject-idxs.onrender.com/Create-Account";
 }
