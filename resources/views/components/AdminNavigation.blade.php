e229d901bdc8027faea015ef2526ff49e017f316
Xkiel07 <101669970+Xkiel07@users.noreply.github.com>
2025-04-21 16:30:40 +0800

    Update AdminNavigation.blade.php

--- resources/views/components/AdminNavigation.blade.php
+++ resources/views/components/AdminNavigation.blade.php
@@ -9,7 +9,10 @@
     {{--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">--}}
     {{-- Fontawesome --}}
     <script src="https://kit.fontawesome.com/6d462838cf.js" crossorigin="anonymous"></script>
+    
+    <script src="https://cdn.tailwindcss.com"></script>
     {{-- Css --}}
+
     <link rel="stylesheet" href="{{asset('AdminAccountCss/AdminAccountBackground.css')}}">
     <link rel="stylesheet" href="{{asset('AdminAccountCss/AdminNavigation.css')}}">
     <link rel="stylesheet" href="{{asset('AdminAccountCss/AdminSidebar.css')}}">
