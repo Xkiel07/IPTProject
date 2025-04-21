54a16a6c0f4b7a11afd4a17dad346f5db9b25e3d
Xkiel07 <101669970+Xkiel07@users.noreply.github.com>
2025-04-22 02:17:19 +0800

    Update AdminNavigation.blade.php

--- resources/views/components/AdminNavigation.blade.php
+++ resources/views/components/AdminNavigation.blade.php
@@ -5,10 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    {{-- Bootstrap --}}
-    <link href="//[::1]:5173/css/app.css" rel="stylesheet">    
-    <script type="module" src="http://[::1]:5173/@vite/client"></script><link rel="stylesheet" href="http://[::1]:5173/resources/sass/app.scss" /><script type="module" src="http://[::1]:5173/resources/js/app.js"></script>    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
     {{--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">--}}
     {{-- Fontawesome --}}
     <script src="https://kit.fontawesome.com/6d462838cf.js" crossorigin="anonymous"></script>
