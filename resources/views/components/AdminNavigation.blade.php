74b7098889dc1eb58ce90e576f8359da478643a8
Xkiel07 <101669970+Xkiel07@users.noreply.github.com>
2025-04-21 16:28:21 +0800

    Update AdminNavigation.blade.php

--- resources/views/components/AdminNavigation.blade.php
+++ resources/views/components/AdminNavigation.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     {{-- Bootstrap --}}
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    {{--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">--}}
     {{-- Fontawesome --}}
     <script src="https://kit.fontawesome.com/6d462838cf.js" crossorigin="anonymous"></script>
     {{-- Css --}}
