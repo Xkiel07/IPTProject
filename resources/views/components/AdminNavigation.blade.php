77e00936cbb22299c72086a00ca54f7d7f9b4cc7
Xkiel07 <101669970+Xkiel07@users.noreply.github.com>
2025-04-22 12:31:56 +0800

    Update AdminNavigation.blade.php

--- resources/views/components/AdminNavigation.blade.php
+++ resources/views/components/AdminNavigation.blade.php
@@ -6,7 +6,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">    
-    @vite(['resources/sass/app.scss','resources/js/app.js'])
+    @vite(['resources/css/app.css','resources/js/app.js'])
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     {{-- Fontawesome --}}
