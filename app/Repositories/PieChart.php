30dc22623998c44a54e376bfacc6bc99cacae990
Xkiel07 <101669970+Xkiel07@users.noreply.github.com>
2025-04-23 18:36:02 +0800

    Update PieChart.php

--- app/Repositories/PieChart.php
+++ app/Repositories/PieChart.php
@@ -6,15 +6,14 @@ use Illuminate\Support\Facades\DB;
 
 class PieChart
 {
-        public function PieChart(){
-            $PieChart = DB::table('patientmedicallog')
-            ->select('Consultation',DB::raw('count(distinct PatientNumber) as NumPatient'))
+    public function PieChart(){
+        $PieChart = DB::table('patientmedicallog')
+            ->select('Consultation', DB::raw('count(distinct "PatientNumber") as NumPatient')) // Added quotes around PatientNumber
             ->whereYear('DateOfConsultation', date('Y'))
             ->whereMonth('DateOfConsultation', date('m'))
             ->groupBy('Consultation')
             ->get();
         
-            return $PieChart;
+        return $PieChart;
     }
-} 
-// in user
+}
