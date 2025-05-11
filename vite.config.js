84a4f59ce3d2d71c058ba738fd55bc415c8b9a0b
Xkiel07 <101669970+Xkiel07@users.noreply.github.com>
2025-05-11 18:52:02 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,20 @@ import tailwindcss from 'tailwindcss';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css','resources/js/app.js',],
+            input: [
+                'resources/sass/app.scss', // Include your SCSS file here
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
     css: {
+        preprocessorOptions: {
+            scss: {
+                // Optional: Include global SCSS variables or mixins
+                // additionalData: '@import "@/resources/sass/_variables.scss";'
+            },
+        },
         postcss: {
             plugins: [
                 tailwindcss(),
